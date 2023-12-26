f2d440fda2b6f70dc6918040ceecdfcfe445afee
Hyuk0510 <hyouk970510@gmail.com>
2023-12-26 10:17:53 +0900

    Initial Commit

---
+++ SeSACWebSocket/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SeSACWebSocket
+//
+//  Created by 선상혁 on 2023/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SeSACWebSocket/SeSACWebSocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeSACWebSocketApp.swift
+//  SeSACWebSocket
+//
+//  Created by 선상혁 on 2023/12/26.
+//
+
+import SwiftUI
+
+@main
+struct SeSACWebSocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
